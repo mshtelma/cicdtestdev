dc5a05c0b1c27b95c793af88b597381e0c2487d7
mshtelma <michael.shtelma@databricks.com>
2020-06-24 11:24:05 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cicdtest_dev',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Databricks Labs CICD Templates Sample Project',
+    author='Thunder Shiviah, Michael Shtelma',
+    license='',
+)
